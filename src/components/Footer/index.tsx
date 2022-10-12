1e4997e63b22bb65dfbe700b5af420a8d4b47f99
Stanislav Melnik <stanislavmelnik@gmail.com>
2022-10-12 17:09:04 +0300

    feat: complete footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,23 @@
+import {
+  AppBar, IconButton, Typography,
+} from '@mui/material';
+import { GitHub } from '@mui/icons-material/';
 import * as style from './style.css';
 
 export const Footer = () => {
+  const onClick = () => {
+    window.open('https://github.com/StanislavMelnik1991');
+  };
   return (
     <footer className={style.wrapper}>
-      footer
+      <AppBar position={'static'} variant='outlined'>
+        <IconButton edge={'start'} color={'inherit'} onClick={onClick}>
+          <GitHub />
+          <Typography variant={'h6'} style={{ paddingLeft: '0.5rem' }}>
+            Stanislau Melnik 2022
+          </Typography>
+        </IconButton>
+      </AppBar>
     </footer>
   );
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,18 +5,16 @@ import {
 
 export const Header = () => {
   return (
-    <header>
-      <AppBar position={'static'}>
-        <Toolbar >
-          <IconButton edge={'start'} color={'inherit'} aria-label={'menu'}>
-            <Menu />
-          </IconButton>
-          <Typography variant={'h6'}>
-            News
-          </Typography>
-          <Button color={'inherit'}>Login</Button>
-        </Toolbar>
-      </AppBar>
-    </header>
+    <AppBar position={'static'}>
+      <Toolbar >
+        <IconButton edge={'start'} color={'inherit'} aria-label={'menu'}>
+          <Menu />
+        </IconButton>
+        <Typography variant={'h6'}>
+          News
+        </Typography>
+        <Button color={'inherit'}>Login</Button>
+      </Toolbar>
+    </AppBar>
   );
 };
