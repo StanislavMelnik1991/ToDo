92191d9fbc30c9c5f5c969584883dffce98e9100
Stanislav Melnik <stanislavmelnik@gmail.com>
2022-10-21 17:37:42 +0300

    fix: eslint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import * as style from './App.css';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
-import { Main } from './components/Main';
 import { Routers } from './router';
 
 export const App = () => {
--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,4 +1,4 @@
-import { Route, Link, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Main } from '../components/Main';
 
 export const Routers = () => {
